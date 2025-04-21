4d4e5759212b676c8d4a2b3cb2064d4eb0587821
AlraweAbdullah <a14alrawi@gmail.com>
2025-04-21 13:43:21 +0200

    add POST endpoint to add a new student

--- src/main/java/be/abdullah/universitysystemspringboot/Controllers/StudentController.java
+++ src/main/java/be/abdullah/universitysystemspringboot/Controllers/StudentController.java
@@ -1,12 +1,13 @@
 package be.abdullah.universitysystemspringboot.Controllers;
 
+import be.abdullah.universitysystemspringboot.dtos.RegisterStudentRequest;
 import be.abdullah.universitysystemspringboot.dtos.StudentDto;
 import be.abdullah.universitysystemspringboot.mapper.StudentMapper;
 import be.abdullah.universitysystemspringboot.repositories.StudentRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -22,4 +23,14 @@ public class StudentController {
         var students = studentRepository.findAll();
         return students.stream().map(studentMapper::toDto).toList();
     }
+
+    @PostMapping
+    public ResponseEntity<StudentDto> registerUser(@RequestBody RegisterStudentRequest request, UriComponentsBuilder builder) {
+        var student = studentMapper.toEntity(request);
+        var studentDto = studentMapper.toDto(studentRepository.save(student));
+
+        var uri = builder.path("/students/{id}").buildAndExpand(student.getId()).toUri();
+        return ResponseEntity.created(uri).body(studentDto);
+    }
+
 }
--- src/main/java/be/abdullah/universitysystemspringboot/mapper/StudentMapper.java
+++ src/main/java/be/abdullah/universitysystemspringboot/mapper/StudentMapper.java
@@ -1,10 +1,12 @@
 package be.abdullah.universitysystemspringboot.mapper;
 
+import be.abdullah.universitysystemspringboot.dtos.RegisterStudentRequest;
 import be.abdullah.universitysystemspringboot.dtos.StudentDto;
 import be.abdullah.universitysystemspringboot.entities.Student;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface StudentMapper {
+    Student toEntity(RegisterStudentRequest request);
     StudentDto toDto(Student student);
 }
