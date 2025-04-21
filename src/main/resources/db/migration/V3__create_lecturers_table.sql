e8876dd53fff17cad97944f358d651cefab9b962
AlraweAbdullah <a14alrawi@gmail.com>
2025-04-21 20:44:58 +0200

    create lecturers table migration

---
+++ src/main/resources/db/migration/V3__create_lecturers_table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE lecturers
+(
+    id             BIGINT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    email          VARCHAR(255) NOT NULL,
+    password       VARCHAR(255) NOT NULL,
+    name           VARCHAR(255) NOT NULL,
+    lastname      VARCHAR(255) NOT NULL
+);
