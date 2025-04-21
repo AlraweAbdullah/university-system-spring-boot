be8b80174809ea0a94dcde21181328767763dc06
AlraweAbdullah <a14alrawi@gmail.com>
2025-04-21 12:58:17 +0200

    init commit

---
+++ src/main/java/be/abdullah/universitysystemspringboot/UniversitySystemSpringBootApplication.java
@@ -0,0 +1,13 @@
+package be.abdullah.universitysystemspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversitySystemSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversitySystemSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/abdullah/universitysystemspringboot/UniversitySystemSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package be.abdullah.universitysystemspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversitySystemSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
