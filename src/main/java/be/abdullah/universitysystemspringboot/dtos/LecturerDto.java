255fadda676b6be0b708849acc7ccef6fc318007
AlraweAbdullah <a14alrawi@gmail.com>
2025-04-23 20:41:25 +0200

    update lecturer dto fields order

--- src/main/java/be/abdullah/universitysystemspringboot/dtos/LecturerDto.java
+++ src/main/java/be/abdullah/universitysystemspringboot/dtos/LecturerDto.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 @Data
 public class LecturerDto {
     private Long id;
-    private String email;
     private String name;
     private String lastname;
+    private String email;
 }
